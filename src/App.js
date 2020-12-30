2957cecc3552c495016f12b548949c6c1aaea2cc
tejas <tejas1188245@gmail.com>
2020-12-30 15:41:10 +0530

    destructuring Completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import './App.css';
 import Props from './Props';
 //import  Counter  from './Counter';
 function App() {
+
+//this method are defined of the destructuring method
+  const employeInformation =[
+    {
+      firstname :"Rathod",
+      lastname :"Tejas",
+      age :"21"
+    },
+    {
+      firstname :"Chuhan" ,
+      lastname :"Akash" ,
+      age :"21"
+    }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +26,18 @@ function App() {
     }
     <h1> Company Directory</h1>
 
-    <Props firstname="Rathod" lastname="Tejas" age="21"/>
+{employeInformation.map((props)=>{
+  const {firstname,lastname,age} =props;
+  return (
+<Props firstname={firstname} lastname={lastname} age={age}/>
+  );
+})}
+  {/*
+  //this are defined of props method.... 
+     <Props firstname="Rathod" lastname="Tejas" age="21"/>
     <Props firstname="Chuhan" lastname="Akash" age="21"/>
+  
+  */}
     </header> 
     </div>
   );
